cfd8892c504ee37e7c5fb14b54b24a52a4a5c970
ssaito-git <67965697+ssaito-git@users.noreply.github.com>
2024-05-22 19:58:05 +0900

    jOOQ のコード生成タスクを修正

--- my-crm-service/data/build.gradle.kts
+++ my-crm-service/data/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     `java-library`
     alias(libs.plugins.flyway)
     alias(libs.plugins.jooq.codegen.gradle)
+    alias(libs.plugins.docker.compose)
     `mycrmservice-detekt`
 }
 
@@ -22,14 +23,14 @@ repositories {
 dependencies {
     implementation(project(":my-crm-service:core"))
 
+    // Spring アノテーションを利用するために追加
+    implementation(libs.spring.context)
+
     // jOOQ
     implementation(libs.jooq)
 
     // PostgreSQL JDBC Driver
-    implementation(libs.postgresql)
-
-    // Spring アノテーションを利用するために追加
-    implementation(libs.spring.context)
+    testRuntimeOnly(libs.postgresql)
 
     // Flyway の Java-based Migrations を利用するために追加
     testImplementation(libs.flyway.core)
@@ -50,20 +51,38 @@ tasks.test {
 sourceSets {
     main {
         kotlin {
-            val outputDirectory = layout.buildDirectory.dir("jooq")
+            val outputDirectory = layout.buildDirectory.dir("generated-jooq")
             srcDir(outputDirectory.get().asFile)
         }
     }
 }
 
+/**
+ * Docker Compose の設定
+ */
+dockerCompose {
+    createNested("postgres").apply {
+        useComposeFiles.addAll(
+            "../../docker/compose.yml",
+            "../../docker/compose.jooq.yml",
+        )
+        startedServices.add("db")
+    }
+}
+
+/**
+ * Flyway の設定
+ */
 flyway {
     url = "jdbc:postgresql://localhost:5432/crm_service"
     user = "postgres"
     password = "secret"
-    cleanDisabled = false
     defaultSchema = "crm_service_schema"
 }
 
+/**
+ * jOOQ の設定
+ */
 jooq {
     configuration {
         jdbc {
@@ -74,7 +93,7 @@ jooq {
         generator {
             name = "org.jooq.codegen.KotlinGenerator"
             target {
-                val outputDirectory = layout.buildDirectory.dir("jooq")
+                val outputDirectory = layout.buildDirectory.dir("generated-jooq")
                 packageName = "mycrmservice.data.jooq"
                 directory = outputDirectory.get().asFile.path
             }
@@ -93,17 +112,59 @@ jooq {
     }
 }
 
-tasks.register<FlywayMigrateTask>("flywayMigrateDevelop") {
-    dependsOn("compileTestKotlin")
-    dependsOn("processTestResources")
-    dependsOn("flywayMigrate")
-    tasks["compileTestKotlin"].mustRunAfter("flywayMigrate")
-    tasks["processTestResources"].mustRunAfter("compileTestKotlin")
-    tasks["flywayMigrateDevelop"].mustRunAfter("processTestResources")
+/**
+ * jOOQ コード生成タスク用の Flyway マイグレーションタスク
+ */
+val jooqFlywayMigrateTask = tasks.register<FlywayMigrateTask>("jooqFlywayMigrate") {
+    user = "postgres"
+    password = "secret"
+    defaultSchema = "crm_service_schema"
+}
+
+/**
+ * jOOQ コード生成タスクのカスタマイズ
+ *
+ * コード生成で利用する DB は Docker で作成する。
+ * 以下の順番でタスクを実行する。
+ *
+ * 1. postgresComposeUp - PostgreSQL のコンテナ起動
+ * 2. jooqFlywayMigrate - Flyway でマイグレーション
+ * 3. jooqCodegen - コード生成
+ * 4. postgresComposeDown - PostgreSQL のコンテナ終了
+ */
+tasks.named("jooqCodegen") {
+    dependsOn("postgresComposeUp")
+    dependsOn("jooqFlywayMigrate")
+    tasks["jooqFlywayMigrate"].mustRunAfter("postgresComposeUp")
+    finalizedBy("postgresComposeDown")
+
+    tasks["postgresComposeUp"].doLast {
+        // ポート番号をランダムで設定する場合は以下の方法で設定されたポート番号を取得する
+        val postgresServiceInfo =
+            dockerCompose.nested("postgres").servicesInfos["db"]?.firstContainer
+                ?: error("postgres container not found.")
+
+        val url = "jdbc:postgresql://localhost:${postgresServiceInfo.ports[5432]}/crm_service"
+
+        jooqFlywayMigrateTask.get().url = url
+
+        jooq.executions.getByName("").configuration.jdbc.url = url
+    }
+}
+
+/**
+ * ローカル開発環境の Flyway マイグレーションタスク
+ *
+ * 通常のマイグレーションと開発データ登録を実行する。
+ */
+tasks.register<FlywayMigrateTask>("localDevFlywayMigrate") {
+    dependsOn("jooqCodegen")
+    dependsOn("testClasses")
+    tasks["compileKotlin"].mustRunAfter("jooqCodegen")
 
     url = "jdbc:postgresql://localhost:5432/crm_service"
     user = "postgres"
     password = "secret"
-    locations = arrayOf("classpath:/testdata")
+    locations = arrayOf("classpath:db/migration", "classpath:/testdata")
     defaultSchema = "crm_service_schema"
 }
