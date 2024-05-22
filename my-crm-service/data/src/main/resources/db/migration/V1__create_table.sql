7cef7d4b4c00ee74bdda66144eb4fc6ede496234
ssaito-git <67965697+ssaito-git@users.noreply.github.com>
2024-05-22 19:55:15 +0900

    created_at, updated_at を追加

--- my-crm-service/data/src/main/resources/db/migration/V1__create_table.sql
+++ my-crm-service/data/src/main/resources/db/migration/V1__create_table.sql
@@ -1,9 +1,27 @@
+-- updated_at の更新トリガー
+create function crm_service_schema.update_updated_at() returns trigger as
+$$
+begin
+    new.updated_at = clock_timestamp();
+    return new;
+end;
+$$
+    language plpgsql;
+
 -- tenants
 create table crm_service_schema.tenants
 (
-    id   UUID primary key,
-    name varchar(100) not null
+    id         UUID primary key,
+    name       varchar(100) not null,
+    created_at timestamptz  not null default clock_timestamp(),
+    updated_at timestamptz  not null default clock_timestamp()
 );
+
+create trigger update_tenants_updated_at
+    before update
+    on crm_service_schema.tenants
+    for each row
+execute procedure crm_service_schema.update_updated_at();
 
 create policy tenant_isolation_policy on crm_service_schema.tenants using (id = (current_setting('app.current_tenant'))::uuid);
 alter table crm_service_schema.tenants
@@ -18,17 +36,33 @@ create table crm_service_schema.token_introspection_client_configs
     client_secret               varchar(100) not null,
     introspection_endpoint      varchar(200) not null,
     issuer_type                 varchar(50)  not null,
+    created_at                  timestamptz  not null default clock_timestamp(),
+    updated_at                  timestamptz  not null default clock_timestamp(),
     primary key (tenant_id, resource_server_domain_name)
 );
+
+create trigger update_token_introspection_client_configs_updated_at
+    before update
+    on crm_service_schema.token_introspection_client_configs
+    for each row
+execute procedure crm_service_schema.update_updated_at();
 
 -- roles
 create table crm_service_schema.roles
 (
-    tenant_id UUID references tenants (id),
-    id        UUID         not null,
-    name      varchar(100) not null,
+    tenant_id  UUID references tenants (id),
+    id         UUID         not null,
+    name       varchar(100) not null,
+    created_at timestamptz  not null default clock_timestamp(),
+    updated_at timestamptz  not null default clock_timestamp(),
     primary key (tenant_id, id)
 );
+
+create trigger update_roles_updated_at
+    before update
+    on crm_service_schema.roles
+    for each row
+execute procedure crm_service_schema.update_updated_at();
 
 create policy tenant_isolation_policy on crm_service_schema.roles using (tenant_id = (current_setting('app.current_tenant'))::uuid);
 alter table crm_service_schema.roles
@@ -42,9 +76,17 @@ create table crm_service_schema.permissions
     resource   varchar(100) not null,
     action     varchar(100) not null,
     is_granted boolean      not null,
+    created_at timestamptz  not null default clock_timestamp(),
+    updated_at timestamptz  not null default clock_timestamp(),
     primary key (tenant_id, role_id, resource, action),
     foreign key (tenant_id, role_id) references roles
 );
+
+create trigger update_permissions_updated_at
+    before update
+    on crm_service_schema.permissions
+    for each row
+execute procedure crm_service_schema.update_updated_at();
 
 create policy tenant_isolation_policy on crm_service_schema.permissions using (tenant_id = (current_setting('app.current_tenant'))::uuid);
 alter table crm_service_schema.permissions
@@ -53,16 +95,24 @@ alter table crm_service_schema.permissions
 -- system_users
 create table crm_service_schema.system_users
 (
-    tenant_id UUID references tenants (id),
-    id        UUID         not null,
-    name      varchar(100) not null,
-    email     varchar(256) not null,
-    password  varchar(100) not null,
-    role_id   UUID         not null,
+    tenant_id  UUID references tenants (id),
+    id         UUID         not null,
+    name       varchar(100) not null,
+    email      varchar(256) not null,
+    password   varchar(100) not null,
+    role_id    UUID         not null,
+    created_at timestamptz  not null default clock_timestamp(),
+    updated_at timestamptz  not null default clock_timestamp(),
     primary key (tenant_id, id),
     unique (tenant_id, email),
     foreign key (tenant_id, role_id) references roles
 );
+
+create trigger update_system_users_updated_at
+    before update
+    on crm_service_schema.system_users
+    for each row
+execute procedure crm_service_schema.update_updated_at();
 
 create policy tenant_isolation_policy on crm_service_schema.system_users using (tenant_id = (current_setting('app.current_tenant'))::uuid);
 alter table crm_service_schema.system_users
@@ -77,9 +127,17 @@ create table crm_service_schema.products
     name        varchar(100) not null,
     description varchar(500) not null,
     active      boolean      not null,
+    created_at  timestamptz  not null default clock_timestamp(),
+    updated_at  timestamptz  not null default clock_timestamp(),
     primary key (tenant_id, id),
     unique (tenant_id, sku)
 );
+
+create trigger update_products_updated_at
+    before update
+    on crm_service_schema.products
+    for each row
+execute procedure crm_service_schema.update_updated_at();
 
 create policy tenant_isolation_policy on crm_service_schema.products using (tenant_id = (current_setting('app.current_tenant'))::uuid);
 alter table crm_service_schema.products
