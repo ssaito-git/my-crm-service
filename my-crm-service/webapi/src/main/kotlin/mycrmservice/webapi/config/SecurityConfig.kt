6fff4cd1b0b7d0ec12174a78a07a9e1ccabdacf0
ssaito-git <67965697+ssaito-git@users.noreply.github.com>
2024-05-30 19:48:01 +0900

    不要な処理を削除

--- my-crm-service/webapi/src/main/kotlin/mycrmservice/webapi/config/SecurityConfig.kt
+++ my-crm-service/webapi/src/main/kotlin/mycrmservice/webapi/config/SecurityConfig.kt
@@ -1,12 +1,6 @@
 package mycrmservice.webapi.config
 
 import jakarta.servlet.http.HttpServletRequest
-import mycrmservice.core.authorization.Action
-import mycrmservice.core.entity.Permission
-import mycrmservice.webapi.authorization.Actor
-import mycrmservice.webapi.authorization.DecisionFunction
-import mycrmservice.webapi.authorization.Scope
-import mycrmservice.webapi.authorization.generateKey
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManagerResolver
@@ -17,8 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.stereotype.Component
-import java.util.UUID
 
 /**
  * セキュリティーコンフィグ
@@ -61,27 +53,4 @@ class SecurityConfig {
         )
         return DelegatingPasswordEncoder(idForEncode, encoders)
     }
-}
-
-data class User(
-    val id: UUID,
-    val name: String,
-    val email: String,
-)
-
-/**
- * ユーザー読み取りの認可判定
- */
-@Component
-class UserReadDecisionFunction : DecisionFunction<Action.Read, User> {
-    override fun allow(actor: Actor, action: Action.Read, resource: User): Boolean {
-        return when (actor) {
-            is Actor.ServiceApplication -> actor.scopes.contains(Scope.USER_READ)
-            is Actor.ServiceUser -> actor.scopes.contains(Scope.USER_READ) && actor.id == resource.id
-            is Actor.SystemApplication -> TODO()
-            is Actor.SystemUser -> actor.role.permissionSet.permitted(Permission.USER_READ)
-        }
-    }
-
-    override fun key() = this.generateKey()
 }
