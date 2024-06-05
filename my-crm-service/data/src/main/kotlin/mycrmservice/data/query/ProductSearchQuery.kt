8f3bfb7cf56cc32e85171eddf76be15d6846891d
ssaito-git <67965697+ssaito-git@users.noreply.github.com>
2024-06-05 18:43:44 +0900

    実装

--- my-crm-service/data/src/main/kotlin/mycrmservice/data/query/ProductSearchQuery.kt
+++ my-crm-service/data/src/main/kotlin/mycrmservice/data/query/ProductSearchQuery.kt
@@ -1,6 +1,10 @@
 package mycrmservice.data.query
 
+import mycrmservice.data.extension.add
+import mycrmservice.data.jooq.tables.references.PRODUCTS
+import org.jooq.Condition
 import org.jooq.DSLContext
+import org.jooq.impl.DSL
 import org.springframework.stereotype.Component
 import java.time.OffsetDateTime
 import java.util.UUID
@@ -9,7 +13,49 @@ import java.util.UUID
 class ProductSearchQuery(
     private val dslContext: DSLContext,
 ) {
-    fun execute(parameter: Parameter) {
+    fun execute(parameter: Parameter): QueryResult {
+        val condition = createCondition(parameter)
+
+        val count = dslContext.fetchCount(PRODUCTS, condition)
+
+        val items = dslContext.selectFrom(PRODUCTS)
+            .where(condition)
+            .offset(parameter.offset)
+            .limit(parameter.limit)
+            .fetchArray()
+            .map {
+                QueryItem(
+                    it.id,
+                    it.sku,
+                    it.name,
+                    it.description,
+                    it.created,
+                    it.updated,
+                )
+            }
+
+        val hasMore = parameter.offset + parameter.limit < count
+
+        return QueryResult(items, hasMore)
+    }
+
+    private fun createCondition(parameter: Parameter): Condition {
+        return DSL.noCondition()
+            .add { c ->
+                parameter.sku?.let {
+                    c.and(PRODUCTS.SKU.contains(it))
+                }
+            }
+            .add { c ->
+                parameter.name?.let {
+                    c.and(PRODUCTS.NAME.contains(it))
+                }
+            }
+            .add { c ->
+                parameter.active?.let {
+                    c.and(PRODUCTS.ACTIVE.eq(it))
+                }
+            }
     }
 
     /**
@@ -18,11 +64,15 @@ class ProductSearchQuery(
      * @property sku SKU
      * @property name 名前
      * @property active プロダクトが有効かどうか
+     * @property limit 取得件数
+     * @property offset オフセット
      */
     data class Parameter(
         val sku: String?,
         val name: String?,
         val active: Boolean?,
+        val limit: Long,
+        val offset: Long,
     )
 
     data class QueryResult(
